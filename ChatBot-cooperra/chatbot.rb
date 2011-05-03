a28dfe6257119a3149605b6292426e22f62eca74
Robbie Cooper <cooperra@rose-hulman.edu>
2011-05-02 23:46:28 -0400

    A new feature

--- ChatBot-cooperra/chatbot.rb
+++ ChatBot-cooperra/chatbot.rb
@@ -2,3 +2,9 @@ STDOUT.sync = true
 puts 'What is your name?'
 name = gets.chomp
 puts "Hello, #{name}!"
+puts "Would you like to play a game?"
+puts "1. Global Thermonuclear War"
+puts "2. Tic-Tac-Toe"
+puts "3. The Game"
+puts "4. Scrabble"
+puts "5. Portal 2"
--- ChatBot-cooperra/features/step_definitions/cli_steps.rb
+++ ChatBot-cooperra/features/step_definitions/cli_steps.rb
@@ -12,3 +12,11 @@ end
 When /^I type "([^\"]*)" and press Enter$/ do |arg1|
 type(arg1)
 end
+
+When /^I see a greeting$/ do
+   assert_partial_output("Hello, .*!")
+end
+
+And /^then see "([^""]*)"$/ do |text|
+assert_partial_output(text)
+end
